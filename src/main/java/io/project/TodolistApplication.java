a15b1ec00f6035d07a945a41011c7df54c4c5046
ashley-stendel <ashley.stendel@logimethods.com>
2017-08-04 16:08:56 -0400

    Change 2

--- src/main/java/io/project/TodolistApplication.java
+++ src/main/java/io/project/TodolistApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodolistApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		SpringApplication.run(TodolistApplication.class, args);
 	}
 }
